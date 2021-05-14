bc27f3541577926eec3d063b979587c06d65f69f
net-runner <slazynski.j@gmail.com>
2021-05-14 09:07:50 +0200

    FCTRL accepted 0.17

---
+++ FCTRL/src/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+fn get_input() -> String {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_line(&mut buffer)
+        .expect("Failed to read line!");
+    buffer
+}
+fn get_uint_from_input() -> u64 {
+    get_input().trim().parse::<u64>().unwrap()
+}
+fn find_zeroes(n: u64) {
+    let mut count: u64 = 0;
+    let mut i: u64 = 5;
+    while n / i > 0 {
+        count += n / i;
+        i *= 5;
+    }
+    println!("{}", count);
+}
+fn main() {
+    let tests = get_uint_from_input();
+
+    for _ in 0..tests {
+        let numbr = get_uint_from_input();
+        find_zeroes(numbr)
+    }
+}
